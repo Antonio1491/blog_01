bcb0ff4e03d6e9df4493c9aace9be97c446227ff
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2023-11-06 16:59:15 -0600

    modificaciones menú adminlte

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\HomeController;
 
 
-Route::get('', [HomeController::class, 'index']);
+Route::get('', [HomeController::class, 'index'])->name('admin.home');
+
+Route::resource('categories', CategoryController::class)->names('admin.categories');
