b4dd4ff18ca89b9800c01602b9bd40d1c93c72ba
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-01-17 17:49:08 -0600

    crud de etiquetas y uso de partials en formularios

---
+++ resources/views/admin/tags/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin - Blog 01')
+
+@section('content_header')
+    <h1>Editar etiqueta</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            {!! Form::model($tag, ['route' => ['admin.tags.update', $tag], 'method'=>'put']) !!}
+                
+                @include('admin.tags.partials.form')
+
+            {!! Form::submit('Actualizar etiqueta', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
